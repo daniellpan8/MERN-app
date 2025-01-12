a897d069270e847c23d30e959a4aa2d8705f528d
Daniell Pan <daniellpan8@gmail.com>
2025-01-12 15:10:01 -0500

    create frontend

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,20 @@
 import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js"
-import Product from "./models/product.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("api/products", async (req,res) => {
-    const product = req.body; // user sends this data
+app.use(express.json()); // middleware
 
-    if(!product.name || !product.price || !product.image){
-        return res.status(400).json({ success:false, message: "Please fill all fields" });
-    }
-
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        res.status(201).json({ success: true, data: newProduct });
-    } catch (error){
-        console.error("Error in creating product", error.message);
-        res.status(500).json({ success: false, message: "Internal Server Error" });
-    }
-});
-
-console.log(process.env.MONGO_URI);
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000");
+    console.log("Server started at http://localhost:" + PORT);
 });
 
 
